968dc9f4179272d09aba838a947151c686333623
IDEKENTERPRISES <32180214+IDEKENTERPRISES@users.noreply.github.com>
2026-08-29 20:02:25 +0100

    Allow socket to be reverse proxied

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('http://localhost:3001');
+const socket: Socket = io({ path: '/socket.io' });
 
 // --- TypeScript Interfaces ---
 interface GuessData {
